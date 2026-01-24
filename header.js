a3c189dea88240c5786cfbeeb606954fa39dd8ba
lefajmofokeng <lefamjack@gmail.com>
2026-01-24 10:35:29 +0200

    Updated

--- header.js
+++ header.js
@@ -297,7 +297,7 @@ class CronosHeader extends HTMLElement {
                 max-width: var(--cronos-content-max-width-fg);
                 width: 100%;
                 margin: 0 auto;
-                padding: 20px 40px 40px 121px;
+                padding: 18px 40px 40px 121px;
             }
 
             .cronos-mega-menu-panel.active {
@@ -877,9 +877,9 @@ class CronosHeader extends HTMLElement {
                         </ul>
                         <div class="cronos-header-action-buttons">
                             
-                            <a href="form.html"><button class="cronos-header-action-btn">
-                                Contact Us
-                            </button></a>
+                            <a href="form.html" class="cronos-header-action-btn">
+                                Contact Cronos
+                            </a>
                             <div class="cronos-qr-dropdown-trigger" id="cronos-qrDropdownTrigger">
                               <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd" clip-rule="evenodd"><path fill="#8fbffa" d="M2 1H1v8h2V3h6V1zm0 22H1v-8h2v6h6v2zM23 1v8h-2V3h-6V1zm0 21v1h-8v-2h6v-6h2z"/><path fill="#2859c5" d="M5 10v1h6V5H5zm4-1H7V7h2zm4 1v1h6V5h-6zm4-1h-2V7h2zM5 19v-6h6v6zm2-2h2v-2H7zm6 .5V19h6v-2h-4v-4h-2zm4-4.5v2h2v-2z"/></g></svg>
                                 <div class="cronos-qr-dropdown-content" id="cronos-qrDropdownContent">
